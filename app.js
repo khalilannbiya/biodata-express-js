0b3088d6c74c508c0b7ed14382d68a726fb92aa8
khalilannbiya <syeichkhalil@gmail.com>
2023-05-11 20:41:17 +0700

    feat: install dotenv, configure .env

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+require("dotenv").config();
 const biodata = require("./biodataRouter");
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use("/biodata", biodata);
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(port, () => {
+  console.log(`Server started on port http://localhost:${port}/`);
 });
